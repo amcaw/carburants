4c2a68a03f315bd9d205cd84065e38659631024c
amcaw <42608053+amcaw@users.noreply.github.com>
2022-03-15 14:15:49 +0100

    Update carbu.py

--- carbu.py
+++ carbu.py
@@ -59,26 +59,29 @@ for row in table_be.tbody.find_all('tr'):
 result = pd.merge(df_be, df_lux, on='carburants')
 result = result.rename(columns={'prix_x': 'Belgique', 'prix_y': 'Luxembourg'})
 
-#On ajoute la date du jour
-
-result['date'] = dt.datetime.today().strftime("%e %B")
-
 #vire euros, espace, virgule, date
 
 result['Belgique'] = result['Belgique'].str.replace(' €/l','')
 result['Belgique'] = result['Belgique'].str.replace(',','.')
 result['Luxembourg'] = result['Luxembourg'].str.replace(' €/l','')
 result['Luxembourg'] = result['Luxembourg'].str.replace(',','.')
-result['date'] = result['date'].str.replace('March','mars')
 
 result['Belgique'] = result['Belgique'].astype(float)
 result['Luxembourg'] = result['Luxembourg'].astype(float)
 
-result = pd.pivot_table(result, values=['Belgique', 'Luxembourg'], index='date',columns='carburants')
+result['prix'] = 'prix'
+
+result = pd.pivot_table(result, values=['Belgique', 'Luxembourg'], index='prix',columns='carburants')
 
 result.columns = result.columns.map('_'.join)
-result.rename_axis('date').reset_index()
+
+#On ajoute la date du jour
+
+result['date'] = dt.datetime.today().strftime("%e %B")
+
+result['date'] = result['date'].str.replace('March','mars')
+
 
 #On exporte en Json en forçant Unicode
 with open('./result.json', 'w') as output_file:
-    result.to_json(output_file, force_ascii=False, orient='records')
+    result.to_json(output_file, force_ascii=False, orient='index')
