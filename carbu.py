c9fee6aee38aa0f9b8d56a431547a570e5c1e4c5
amcaw <42608053+amcaw@users.noreply.github.com>
2022-03-16 16:13:13 +0100

    Update carbu.py

--- carbu.py
+++ carbu.py
@@ -36,8 +36,7 @@ for row in table_lux.tbody.find_all('tr'):
         
 #Nettoyage pour merge après
 
-df_lux['carburants'] = df_lux['carburants'].str.replace('Super 95','Super 95 (E5)')
-df_lux['carburants'] = df_lux['carburants'].str.replace('Super 98','Super 98 (E5)')
+df_lux['carburants'] = df_lux['carburants'].str.replace('Super 95','Super 95 (E10)')
 df_lux['carburants'] = df_lux['carburants'].str.replace('Diesel','Diesel (B7)')
 
 #Collecting Data Be
@@ -59,7 +58,7 @@ for row in table_be.tbody.find_all('tr'):
 result = pd.merge(df_be, df_lux, on='carburants')
 result = result.rename(columns={'prix_x': 'Belgique', 'prix_y': 'Luxembourg'})
 
-#vire euros, espace, virgule, date
+#on vire euros, espace, virgule, date, quelques modifs en plus
 
 result['Belgique'] = result['Belgique'].str.replace(' €/l','')
 result['Belgique'] = result['Belgique'].str.replace(',','.')
@@ -75,13 +74,15 @@ result = pd.pivot_table(result, values=['Belgique', 'Luxembourg'], index='prix',
 
 result.columns = result.columns.map('_'.join)
 
-#On ajoute la date du jour
+#On ajoute la date du jour et d'autres brols
 
 result['date'] = dt.datetime.today().strftime("%e %B")
 
 result['date'] = result['date'].str.replace('March','mars')
 
+result = result.rename(columns={'Belgique_Diesel (B7)': 'Belgique_Diesel', 'Belgique_Super 95 (E10)': 'Belgique_Super_95', 'Luxembourg_Diesel (B7)' : 'Luxembourg_Diesel', 'Luxembourg_Super 95 (E10)' : 'Luxembourg_Super_95' })
 
-#On exporte en Json en forçant Unicode
+
+#On exporte en Json en forçant Unicode avec un chipotage pour virer les square brackets
 with open('./result.json', 'w') as output_file:
-    result.to_json(output_file, force_ascii=False, orient='records', indent=2)
+    result.to_json(output_file, force_ascii=False, orient='records', lines=True)
