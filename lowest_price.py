ee133b119d7eb0969cde960a370cbcbf7e7c96c9
amcaw <42608053+amcaw@users.noreply.github.com>
2022-11-17 08:28:41 +0100

    Update lowest_price.py

--- lowest_price.py
+++ lowest_price.py
@@ -10,7 +10,7 @@ soup = BeautifulSoup(data, 'html.parser')
 table = soup.find('table', class_='table table-bordered')
 
 # Defining of the dataframe
-df = pd.DataFrame(columns=['province', 'Super 95 (E10)', 'Super 98 (E5)', 'Diesel (B7)'])
+df = pd.DataFrame(columns=['Province', 'Super 95 (E10)', 'Super 98 (E5)', 'Diesel (B7)'])
 
 # Collecting Ddata
 for row in table.find_all('tr'):    
@@ -18,11 +18,11 @@ for row in table.find_all('tr'):
     columns = row.find_all('td')
     
     if(columns != []):
-        province = columns[0].text.strip()
+        Province = columns[0].text.strip()
         E95 = columns[1].text.strip()
         E98 = columns[3].text.strip()
         D7 = columns[5].text.strip()
 
-        df = df.append({'Province': province,  'Super 95 (E10)': E95, 'Super 98 (E5)': E98, 'Diesel (B7)' : D7}, ignore_index=True)
+        df = df.append({'Province': Province,  'Super 95 (E10)': E95, 'Super 98 (E5)': E98, 'Diesel (B7)' : D7}, ignore_index=True)
 
 df.to_csv("./best_carbu.csv", index=False)
