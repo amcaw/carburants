f4b839994d0342b6e2e658a1c6d5f8a3876c7f0a
amcaw <42608053+amcaw@users.noreply.github.com>
2022-11-17 08:17:28 +0100

    Update lowest_price.py

--- lowest_price.py
+++ lowest_price.py
@@ -23,16 +23,6 @@ for row in table.find_all('tr'):
         E98 = columns[3].text.strip()
         D7 = columns[5].text.strip()
 
-        df = df.append({'province': province,  'Super 95 (E10)': E95, 'Super 98 (E5)': E98, 'Diesel (B7)' : D7}, ignore_index=True)
+        df = df.append({'Province': province,  'Super 95 (E10)': E95, 'Super 98 (E5)': E98, 'Diesel (B7)' : D7}, ignore_index=True)
 
-df_E95 = df[["province", "Super 95 (E10)"]]
-df_E98 = df[["province", "Super 98 (E5)"]]
-df_D7 = df[["province", "Diesel (B7)"]]
-
-df_E95.sort_values(by=['Super 95 (E10)'], inplace=True)
-df_E98.sort_values(by=['Super 98 (E5)'], inplace=True)
-df_D7.sort_values(by=['Diesel (B7)'], inplace=True)
-
-df_E95.to_csv("./E95.csv", index=False)
-df_E98.to_csv("./E98.csv", index=False)
-df_D7.to_csv("./D7.csv", index=False)
+df.to_csv("./best_carbu.csv", index=False)
